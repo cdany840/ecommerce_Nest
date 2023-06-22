ef44998e4aa463d19872220c2dc2626ad6409b7c
Luis Perez <LuisM1P3@hotmail.com>
2023-06-21 20:16:36 -0600

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  /* app.enableCors();
   app.enableCors(
     { 
       origin: ['http://localhost:4200/login'],
     }
-  );
+  ); */
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.use('/img', express.static(join(__dirname, '..', 'img')));
