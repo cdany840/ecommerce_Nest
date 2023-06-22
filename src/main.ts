2e1cdde549a7fecb249b78b219c51df33f9f8cf3
Luis Perez <LuisM1P3@hotmail.com>
2023-06-21 22:45:32 -0600

    origin cors

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.enableCors(
-    { 
-      origin: ['https://backend-nestjs-seven.vercel.app/login'],
-    }
-  );
-
-  /* const corsOptions: CorsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+  app.use('/auth', cors({
+    origin: 'https://ecommerce-a16.vercel.app',
     credentials: true,
-  };
-  app.enableCors(corsOptions); */
+    }));
+
+  app.use('/uploads', cors({
+    origin: 'https://ecommerce-a16.vercel.app',
+    credentials: true,
+  }));
+
+  app.use('/img', cors({
+    origin: 'https://ecommerce-a16.vercel.app',
+    credentials: true,
+  }));
+
+  app.use('/img-category', cors({
+    origin: 'https://ecommerce-a16.vercel.app',
+    credentials: true,
+  }));
+
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.use('/img', express.static(join(__dirname, '..', 'img')));
